dc3bbaaffcecbd2b5d2099ab07eed63d316029fd
Scott Bowler <scott.bowler@learningfuze.com>
2018-11-16 17:28:37 -0800

    Completed getting list data from server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,15 @@
 import types from './types';
+import axios from 'axios';
 
+const BASE_URL = 'http://api.reactprototypes.com/todos';
+const API_KEY = '?key=c918_demouser';
+
+export function getListData(){
+
+    const resp = axios.get(BASE_URL + API_KEY);
+
+    return {
+        type: types.GET_ALL_LIST_DATA,
+        payload: resp
+    }
+}
